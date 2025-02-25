35adbca5d5ed8c2a45cf68e741aae7c0dcb2bf84
schrodj1 <158081339+schrodj1@users.noreply.github.com>
2025-02-25 11:46:36 -0500

    Delete lg directory

--- lg/actuator.cpp
+++
@@ -1,1 +0,0 @@
-//this will take commands from the master and send commands to the legs. It will also publish the leg position based on feedback
--- lg/src/comProc.cpp
+++
@@ -1,1 +0,0 @@
-//this will process what the leg commands SHOULD be given the plane. It will send those commands to the master node
